3634bd39beb57b72d23259aad4929188440dc3a5
Nippon <37734266+nnippon@users.noreply.github.com>
2021-02-15 15:43:54 +0200

    Update ubuntu-run.sh

--- ubuntu-run.sh
+++ ubuntu-run.sh
@@ -1,1 +1,3 @@
-
+git clone --recurse-submodules https://github.com/nnippon/ErfanGSIs.git gsi && git clone https://github.com/akhilnarang/scripts
+sudo apt update && sudo apt upgrade -y
+bash scripts/setup/android_build_env.sh && bash gsi/setup.sh
